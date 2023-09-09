f21faa3bb0c8e8c58967e7d42d8777e635d3cc2e
Yantong_zhang <1678532032@qq.com>
2023-09-09 23:26:41 +0800

    feat:调整ViewModel的处理逻辑 fix:NA

--- app/src/main/java/com/example/wanandroid/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/wanandroid/ui/home/HomeViewModel.kt
@@ -1,67 +1,36 @@
 package com.example.wanandroid.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.wanandroid.domain.PexelsResourceRepository
 import com.example.wanandroid.domain.WanAndroidRepository
 import com.example.wanandroid.domain.bean.Article
-import com.example.wanandroid.domain.bean.PexelPhoto
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val wanAndroidRepository: WanAndroidRepository,
     private val pexelsResourceRepository: PexelsResourceRepository
 ) : ViewModel() {
 
-    private val _articles: MutableLiveData<List<Article>> = MutableLiveData()
-    val articles: LiveData<List<Article>> = _articles
+    suspend fun getArticles(): Flow<List<Article>> {
+        // TODO: 这个perPage需要调整获取方法
+        val perPage = wanAndroidRepository.getNumOfOriginArticles().first()
 
-    private val _pexelPhotos: MutableLiveData<List<PexelPhoto>> = MutableLiveData()
-    val pexelPhotos: LiveData<List<PexelPhoto>> = _pexelPhotos
-
-    init {
-        getArticles()
-    }
-
-    private fun getArticles() {
-        viewModelScope.launch {
-            val originArticlesFlow = wanAndroidRepository.getOriginArticles()
-            val perPage = wanAndroidRepository.getNumOfOriginArticles().first()
-            val pexelsFlow = pexelsResourceRepository.getPexelPhotos("android", perPage)
-
-            originArticlesFlow.combine(pexelsFlow) { originArticles, pexels ->
-                val articles = mutableListOf<Article>()
-                for (i in originArticles.indices) {
-                    articles += Article(
-                        title = originArticles[i].title,
-                        author = originArticles[i].author,
-                        imageUrl = pexels[i].src?.original!!
-                    )
-                }
-                articles
-            }.collectLatest {
-                _articles.postValue(it)
+        return combine(
+            wanAndroidRepository.getOriginArticles(),
+            pexelsResourceRepository.getPexelPhotos("android", perPage)
+        ) { originArticles, pexels ->
+            val articles = mutableListOf<Article>()
+            for (i in originArticles.indices) {
+                articles += Article(
+                    title = originArticles[i].title,
+                    author = originArticles[i].author,
+                    imageUrl = pexels[i].src?.original!!
+                )
             }
+            articles
         }
     }
 
-    private fun getPexelPhotos(
-        query: String,
-        perPage: Int
-    ) {
-        viewModelScope.launch {
-            pexelsResourceRepository.getPexelPhotos(
-                query = query,
-                perPage = perPage
-            ).collectLatest {
-                _pexelPhotos.postValue(it)
-            }
-        }
-    }
 }
