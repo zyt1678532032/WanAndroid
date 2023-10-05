f8f80e073c4dd63f17d45899ea89b8581914fa6d
Yantong_zhang <1678532032@qq.com>
2023-10-05 13:32:03 +0800

    feat:buildSrc依赖管理

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,28 @@
+object Versions {
+    const val constraintlayout = "2.1.4"
+    const val androidx = "1.8.0"
+    const val appcompat = "1.6.1"
+    const val lifecycle = "2.6.1"
+    const val navigation = "2.6.0"
+
+    const val material = "1.9.0"
+
+    const val pluto = "2.1.6"
+
+    const val retrofit = "2.9.0"
+    const val gson = "2.9.0"
+    const val shimmer = "0.5.0"
+    const val compose = "1.4.0"
+    const val compose_material3 = "1.1.0"
+    const val eventbus = "3.3.1"
+
+    const val glide = "4.12.0"
+
+    const val junit = "4.13.2"
+
+    const val androidxTestExt = "1.1.3"
+    const val androidxTestEspresso = "3.5.1"
+
+    const val kotlinxCoroutinesTest = "1.7.3"
+
+}
